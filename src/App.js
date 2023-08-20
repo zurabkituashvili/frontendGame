aa735e9a873ed1bb1559abf76a9b501e3101ad51
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-08-20 19:54:05 +0400

    gameeeee

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Home from "./components/Home/Home";
+import CreateRoom from "./components/CreateRoom/CreateRoom";
+import JoinRoom from "./components/JoinRoom/JoinRoom";
+import Game from "./components/Game/Game";
+import NotFound from "./components/NotFound/NotFound";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import io from "socket.io-client";
 
+const socket = io.connect("http://localhost:3001");
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route
+              path="/createRoom"
+              element={<CreateRoom socket={socket} />}
+            ></Route>
+            <Route
+              path="/joinRoom"
+              element={<JoinRoom socket={socket} />}
+            ></Route>
+            <Route
+              path="/game/:roomId"
+              element={<Game socket={socket} />}
+            ></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </Router>
       </header>
     </div>
   );
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import image from "../../image.png";
+
+const Logo = () => {
+  return (
+    <div>
+      <img src={image} className="App-logo" alt="logo" />
+    </div>
+  );
+};
+
+export default Logo;
